55744bd3b5c7fcd506b9302ad39eef4426f03d9f
Dennis <Dennis@DESKTOP-V63LC0R>
2017-12-28 14:18:05 +0900

    주석 변경 테스트

--- src/Dennis/Sungtaek/main.java
+++ src/Dennis/Sungtaek/main.java
@@ -28,8 +28,8 @@ public class main {
 		System.out.println("4. 눗셈");
 
 		choice = sc.next().charAt(0); //Scanner에서 받을 때 char형은 따로 받는 .nextChar()이 없기 때문에
-									 //String 입력인 .next()로 받고, charAt(index)를 통해 받는다. 0번째 입력란을 받음
-									 //즉, choice에 '123'을 입력할 경우 1번이 선택. '314'을 입력하면 3번 선택
+							 //String 입력인 .next()로 받고, charAt(index)를 통해 받는다. 0번째 입력란을 받음
+							 //즉, choice에 '123'을 입력할 경우 1번이 선택. '314'을 입력하면 3번 선택
 
 		switch(choice) {
 		case '1':
